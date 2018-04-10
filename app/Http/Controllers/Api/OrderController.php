e383cebd597aa68d8b45821734d698f47cd38da8
1946431302@qq.com <1946431302@qq.com>
2018-04-10 17:58:26 +0800

    'adout_redis_purchase'

--- app/Http/Controllers/Api/OrderController.php
+++ app/Http/Controllers/Api/OrderController.php
@@ -9,6 +9,7 @@ use App\Http\Controllers\Controller;
 use App\Models;
 use Illuminate\Contracts\Redis;
 use Illuminate\Support\Facades\Cache;
+use Log;
 
 class OrderController extends Controller
 {
@@ -59,15 +60,65 @@ class OrderController extends Controller
         event(new UpdateOrder($event));
     }
 
+    /**
+     * Redis抢购
+     * @param Request $request
+     * @return string
+     */
     public function rushPurchase(Request $request)
     {
         $id = $request->id;
         // 检查是否纯在redis 没有则没有开始
         if (Cache::has($id)) {
-
-
+            $buyNum = rand(1, 10);        // 随机产生抢购数额
+            $userId = rand(1, 200000);    // 随机产生用户id
+            // 检查剩余数量是否足够
+            $num = Cache::decrement($id, $buyNum);
+            if ($num < 0) {
+                Cache::increment($id, $buyNum);
+                return parent::error('剩余份数足,抢购失败');
+            } else {
+                //删除关于这个商品抢购记录
+                if (Cache::has($id . $userId)) {
+                    Cache::forget($id . $userId);
+                }
+                if (Cache::add($id . $userId, $buyNum, 20)) {
+                    Log::info('缓存错误');
+                    return parent::error('网络延迟');
+                } else {
+                    return static::redisCreateOrder(id . $userId, $buyNum, $userId, $id);
+                    //return parent::error('抢购成功');
+                }
+            }
         } else {
             return parent::error('还没有开始抢购');
+        }
+    }
+
+    /**
+     * 创建生订单
+     * @return string
+     */
+    private static function redisCreateOrder($key, $num, $uId, $gId)
+    {
+        if (Cache::has($key)) {
+            $mes = new Models\Message();
+            $bool = $mes->insert([
+                'message_title' => '成功了耶' . rand(100, 200),
+                'message_text' => 'good' . $gId,
+                'message_time' => $_SERVER['REQUEST_TIME'],
+                'message_form' => '哈哈哈' . $num,
+                'user_id' => $uId,
+                'admin_id' => 2
+            ]);
+            if ($bool) {
+                return parent::success('购买成功');
+            } else {
+                return parent::error('购买失败');
+            }
+        } else {
+            Log::info('失败2');
+            return parent::error();
         }
     }
 
